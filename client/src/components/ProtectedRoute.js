281691ede819d473b8aa041707b6653ad1d9a2be
Slavik_sviy <124872079+SlavikSvoy@users.noreply.github.com>
2026-06-12 17:04:52 +0300

    Update ProtectedRoute.js

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -4,7 +4,7 @@ export default function ProtectedRoute({ children, roles }) {
   const userRole = localStorage.getItem("role");
 
   if (!userRole) {
-    return <Navigate to="/staff-login" replace />;
+    return <Navigate to="/login" replace />;
   }
 
   if (roles && !roles.includes(userRole)) {
