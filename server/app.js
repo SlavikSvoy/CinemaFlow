ab76f6c10a5a6330b7310a2813ebf94540cd7898
Slavik_sviy <124872079+SlavikSvoy@users.noreply.github.com>
2026-06-16 01:19:11 +0300

    Refactor server setup with dotenv and port config
    Added dotenv configuration and updated server port handling.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -10,11 +12,14 @@ app.use("/movies", require("./routes/movieRoutes"));
 app.use("/sessions", require("./routes/sessionRoutes"));
 app.use("/bookings", require("./routes/bookingRoutes"));
 app.use("/checkin", require("./routes/checkinRoutes"));
+app.use("/controller", require("./routes/controllerRoutes"));
 
 app.get("/", (req, res) => {
   res.send("CinemaFlow API працює");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
